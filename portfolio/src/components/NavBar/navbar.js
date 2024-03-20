602261e159212727776742ae53c8e53989a758a6
denisa122 <neagudenisa12@yahoo.com>
2024-03-20 20:56:28 +0100

    burger menu for mobile implemented

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import {Link} from 'react-scroll';
 
 import logo from '../../assets/Denisa Neagulogo.svg';
-import contactImg from '../../assets/contactImg.png'
+import contactImg from '../../assets/contactImg.png';
+import menu from '../../assets/burgernav.png';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
    <nav className='navbar'>
         <img src={logo} alt="logo" className='logo'/>
         <div className='desktopMenu'>
-            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
-            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>About</Link>
-            <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Portfolio</Link>
-            <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Experience</Link>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Home</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>About</Link>
+            <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Portfolio</Link>
+            <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Experience</Link>
         </div>
         <button className='desktopMenuBtn' onClick={() => {
           document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
         }}>
             <img src={contactImg} alt="contact icon" className='desktopMenuImg'/>Contact me
         </button>
+
+        <img src={menu} alt="menu" className='mobileMenu' onClick={() => setShowMenu(!showMenu)}/>
+        <div className='navMenu' style={{display: showMenu ? 'flex' : 'none'}}>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Home</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>About</Link>
+            <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Portfolio</Link>
+            <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Experience</Link>
+            <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={() => setShowMenu(false)}>Contact</Link>
+        </div>
    </nav>
   )
 }
